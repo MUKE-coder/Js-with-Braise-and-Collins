1c620b4b84c878eb9109ec5aa7ae263932f8b828
MUKE-coder <gmukejohnbaptist@gmail.com>
2023-04-10 11:31:37 +0300

    Initial commit

---
+++ Data types/app.js
@@ -0,0 +1,27 @@
+// console.log("Hello world!");
+// DATA TYPES
+/* 
+1) Numbers => 25, 3000, 15.6 -10
+2) strings => "Any text or characters"
+3)Boolean => true or false
+
+
+
+VARIABLES
+*/
+const age = 25;
+const myName = "JB";
+let marks = 0;
+var score = 0;
+let averageMarks;
+const country = "Ugandan";
+const email = "test@gmail.com";
+const isMarried = false;
+const hasChildren = true;
+const isOrphan = false;
+
+console.log(typeof email);
+// OPERATORS
+const x = 3;
+const y = 2;
+console.log(y ** x);
