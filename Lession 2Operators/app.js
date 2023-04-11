d94db46b1f9b91d6e3a3481a3aa6ba92f560c2c1
MUKE-coder <gmukejohnbaptist@gmail.com>
2023-04-11 11:56:38 +0300

    Lession two

---
+++ Lession 2Operators/app.js
@@ -0,0 +1,24 @@
+// const y = 100;
+
+// console.log(x > 30 && y > 100 );
+// console.log(x > 30 || y > 100);
+// console.log(!(x > 30));
+const x = 10;
+if (x > 18) {
+  // if condition is true
+  console.log("X is above 18");
+} else {
+  console.log("X is below is 18");
+}
+const usernameLength = 9;
+if (usernameLength >= 3 && usernameLength <= 8) {
+  console.log("Username is Okay");
+} else {
+  console.log("Username Must be between 3 and 8 characters");
+}
+
+if (x % 2 == 0 && x > 10) {
+  console.log("X is Okay");
+} else {
+  console.log("X is Invalid");
+}
