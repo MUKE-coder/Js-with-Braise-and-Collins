1c6f5353ad506d3c7f7e2db17738e46ad6c921c5
MUKE-coder <gmukejohnbaptist@gmail.com>
2023-04-26 16:52:27 +0300

    Loops

---
+++ Loops/app.js
@@ -0,0 +1,14 @@
+// for (let student = 1; student <= 10; student++) {
+//   console.log(student);
+// }
+
+// let i = 1;
+// while (i <= 100) {
+//   console.log(i);
+//   i++;
+// }
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let num of numbers) {
+  console.log(num ** 2);
+}
