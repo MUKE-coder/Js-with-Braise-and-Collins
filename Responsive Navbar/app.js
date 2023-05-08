a50a974b7eb4a337a7d22b222d3a5b6fdd0d5d3d
MUKE-coder <gmukejohnbaptist@gmail.com>
2023-05-08 12:13:14 +0300

    responsive navbar

---
+++ Responsive Navbar/app.js
@@ -0,0 +1,11 @@
+const humbergBtn = document.querySelector(".humberg-menu-btn");
+const menu = document.querySelector(".nav__list");
+console.log(menu);
+humbergBtn.addEventListener("click", () => {
+  // if (menu.classList.contains("showMenu")) {
+  //   menu.classList.remove("showMenu");
+  // } else {
+  //   menu.classList.add("showMenu");
+  // }
+  menu.classList.toggle("showMenu");
+});
